fa000adbbaeb7c7f0518be5bcc746f90a38b2e2e
Atara Razin <atararazin@gmail.com>
2020-04-05 13:53:22 +0300

    changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ 'hi' : 'there'});
+    res.send({ 'hi' : 'there, atara'});
 });
 
 const PORT = process.env.PORT || 5000 //so that heroku can pass port dynamically. default port : 5000
